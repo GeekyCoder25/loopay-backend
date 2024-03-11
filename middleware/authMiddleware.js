36e83206cf231a1ae44f44044701f87fa1e2dc3a
Toyyib Lawal <toyibe25@gmail.com>
2024-03-11 09:00:36 +0100

    bug fixes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -31,10 +31,7 @@ const protect = async (req, res, next) => {
 				const sessionsNotToUpdate = previousSessionsData.filter(
 					session => session.deviceID !== req.sessionID
 				);
-				if (
-					sessionToUpdate.length === 1 &&
-					sessionToUpdate[0].status === 'active'
-				) {
+				if (sessionToUpdate.length === 1) {
 					const lastSeen = new Date();
 					let session = {...sessionToUpdate[0], lastSeen};
 					req.sessionTime = lastSeen;
